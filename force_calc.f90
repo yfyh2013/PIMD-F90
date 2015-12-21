220be9c6e6bcf1a362ac17b49bc73822583b1590
DAN <delton17@gmail.com>
2015-12-21 16:36:02 -0500

    rm cleaning of .out, add input files for 64mols

--- force_calc.f90
+++ force_calc.f90
@@ -15,11 +15,11 @@ subroutine full_bead_forces
  
  do bat = 0, Nbatches - 1 !batch index
 
-	if ((pot_model .eq. 6).and.(mod(t,2000).eq.0).and.(t.gt.0)) then 
-		call io_open(lun_clean,trim(sys_label)//trim(str(pid))//".out",REPLACE=.true.) !clean out .out files
-		write(lun_clean) " "
-		call io_close(lun_clean)
-	endif 
+	!if ((pot_model .eq. 6).and.(mod(t,2000).eq.0).and.(t.gt.0)) then 
+	!	call io_open(lun_clean,trim(sys_label)//trim(str(pid))//".out",REPLACE=.true.) !clean out .out files
+	!	write(lun_clean) " "
+	!	call io_close(lun_clean)
+	!endif 
 	
     !we want to send a (3 x Natoms) array to each processor 
     if (pid .eq. 0) then
