1e52d977573a4d2aa0f6b7aa0ae6367df3f9fa81
DAN <delton17@gmail.com>
2015-12-21 11:28:41 -0500

    m fix

--- force_calc.f90
+++ force_calc.f90
@@ -12,7 +12,7 @@ subroutine full_bead_forces
 
  do bat = 0, Nbatches - 1 !batch index
 
-	if ((pot_model .eq. 6).and.(mod(t,2000).eq.0)) then 
+	if ((pot_model .eq. 6).and.(mod(t,2000).eq.0).and.(t.gt.0)) then 
 		call system("rm "//trim(sys_label)//trim(str(pid))//".out") !clean out .out files
 	endif 
 	
